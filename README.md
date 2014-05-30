0581687c7b4b5ebaa4e6fd2a7c3b1567b7328b03
zhaosummer <summerzhao.idea@gmail.com>
2014-05-30 10:42:08 +0800

    refine the project to maven and better format

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 demo_nlp_im
 ===========
-
 interest mining demo
+
+1. LDA example based on Gibbs sampling. 
+
+This demo use IKAnalyzer as a chinese word segmentation. Since the project is a MAVEN project, but IKAnalyzer is not added to the global MAVEN repo, please add the ik-analyzer.jar in dependencies folder to your local MAVEN repo using the following command:
+
+'''
+mvn install:install-file -Dfile=ik-analyzer.jar -DgroupId=org.wltea.ik-analyzer -DartifactId=ik-analyzer -Dversion=3.2.8 -Dpackaging=jar
+'''
--- lda/src/main/java/liuyang/nlp/lda/com/ChineseTraditionalTokenizator.java
+++ lda/src/main/java/liuyang/nlp/lda/com/ChineseTraditionalTokenizator.java
@@ -1,29 +1,27 @@
 package liuyang.nlp.lda.com;
 
+import org.wltea.analyzer.IKSegmentation;
+import org.wltea.analyzer.Lexeme;
+
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
-
-
-import org.wltea.analyzer.core.IKSegmenter;
-import org.wltea.analyzer.core.Lexeme;
-import org.wltea.analyzer.lucene.IKAnalyzer;
 
 
 
 public class ChineseTraditionalTokenizator {
 
 public static ChineseTraditionalTokenizator INSTANCE = new ChineseTraditionalTokenizator();
-	
+
 
 	private ChineseTraditionalTokenizator(){
-		
+
 	}
 	
 	public static void tokenizeAndLowerCase(String line, ArrayList<String> tokens) throws IOException
 	{
 		StringReader sr=new StringReader(line);
-		IKSegmenter iks=new IKSegmenter(sr, true);
+		IKSegmentation iks=new IKSegmentation(sr, true);
 		Lexeme lex=null;
 		while((lex=iks.next())!=null){
 			//System.out.print(lex.getLexemeText()+"|");
@@ -38,17 +36,17 @@ public static ChineseTraditionalTokenizator INSTANCE = new ChineseTraditionalTok
 		//ChineseTraditionalTokenizator ctt = new ChineseTraditionalTokenizator();
 		String str="习近平指出日本在此事件中的强词夺理在其矛盾说辞中可见一斑。据共同社报道，日本外务省事务次官斋木昭隆26日召见中国驻日本大使程永华，就军机接近一事提出抗议。";
 		StringReader sr=new StringReader(str);
-		IKSegmenter ik=new IKSegmenter(sr, true);
+        IKSegmentation ik=new IKSegmentation(sr, true);
 		Lexeme lex=null;
 		while((lex=ik.next())!=null){
 			System.out.print(lex.getLexemeText()+"|");
 		}
 		sr.close();
 		System.out.println();
-		
+
 		//System.out.println(ctt.getTokenList(str).toString());
 		//List<String> textTokenList = ChineseTraditionalTokenizator.INSTANCE.getTokenList(str);
-		
+
 		//System.out.println(textTokenList);
 		
 
